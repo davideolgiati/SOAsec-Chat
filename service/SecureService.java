a11ac8a8ab103bf887233f6bcd6585186946dce2
Deiv <dave.tho@protonmail.com>
2020-07-06 14:41:40 +0000

    aggiunti metodi per la creazione, inserimento e rimozione da una coda

--- service/SecureService.java
+++ service/SecureService.java
@@ -35,11 +35,73 @@ public class SecureService {
 
     private Map<String, Integer> offsets = new Map<String, Integer>();
 
+    // Per comodità tengo anche una variabile che mi dice quanti
+    // elementi ho nella lista
+
+    private int ListSize = 0;
+
+    // Per aggiornare lo stato delle code (sia push che pop) dobbiamo
+    // salvare le code originali in una variabile temporanea, aggiornare
+    // e poi riscrivere i valori finali
+
+    // STEP
+
+    // 1. LEGGO LA CODA
+    // 2. MODIFICO
+    // 3. SE LA MODIFICA È COMPLETA VAI A 4. ALTRIMENTI TORNA 2.
+    // 4. SCRIVO IL VALORE MODIFICATO NELLA POSIZIONE ORIGINALE
+
     // Metodo privato per l'inserimento di un valore nella coda
     // all'offset specificato
-    private boolean QueuePush(int offset){
-	// TODO
-	// so gia come farlo pregno, lascialo a me
+    private boolean QueuePush(int offset, String value){
+	// Salvo l'elemento da modificare nella variabile tmp
+	Queue<String> tmp = code.get(offset);
+	// dato che il metodo add ritorna un valore booleano, uso
+	// questo valore come indicatore di successo dell'operazione
+	// di aggiornamneto
+	boolean result = tmp.add(value);
+	if (result) {
+	    // Solo se l'operazione è andata a buon fine aggiorno la
+	    // lista
+
+	    // Dato che anche l'update della lista può fallire, metto
+	    // in and logico i risultati delle due operazioni per
+	    // verificare l'esito complessivo dell'aggiornamento
+	    result &= code.set(offset, tmp);
+	}
+	return result;
+    }
+
+    // Metodo privato per la lettura del primo valore nella coda
+    // all'offset specificato
+    private boolean QueuePull(int offset){
+	// Salvo l'elemento da modificare nella variabile tmp
+	Queue<String> tmp = code.get(offset);
+	// dato che il metodo add ritorna un valore booleano, uso
+	// questo valore come indicatore di successo dell'operazione
+	// di aggiornamneto
+	String value = tmp.poll();
+	if (result != null) {
+	    // Solo se l'operazione è andata a buon fine aggiorno la
+	    // lista
+	    code.set(offset, tmp);
+	} else {
+	    value = ""
+	}
+	return value;
+    }
+
+    private boolean CreateQueue(String user){
+	if (!offsets.containsKey(User)) {
+	    Queue<String> newQueue = new Queue<String>();
+	    boolean res = code.add(newQueue);
+	    if (res) {
+		ListSize++;
+		res &= offsets.add(user, ListSize);
+	    }
+	    return res;
+	}
+	return false;
     }
 
     // API per l'invio di un messaggio
