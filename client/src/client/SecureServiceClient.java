f9932b244f429c9a599433767680f9bb23378dfe
TheIv <dave.tho@protonmail.com>
2020-10-08 14:31:17 +0000

    aggiunto help

--- client/src/client/SecureServiceClient.java
+++ client/src/client/SecureServiceClient.java
@@ -37,6 +37,50 @@ public class SecureServiceClient {
     listener.start();
   }
 
+  // funzione help
+  private static String help() {
+    // compongo il banner
+    String text = "Benvenuto in SOAsec-Chat!\n";
+    text += "Eccoti le azioni possibili:"; 
+    text += "(digitare il codice corrisponente all'azione)\n\n";
+    text += "CODICE\tAZIONE\n";
+    text += ":h\tmostra questo banner\n";
+    text += ":c\tinizia a chattare\n";
+    text += ":e\tesci\n\n\n>";
+
+    // inizializzo la tastiera
+    Scanner keyboard = new Scanner(System.in);
+    // leggo la risposta
+    String res = keyboard.next();
+    // chiudo la tastiera
+    keyboard.close();
+    // ritorno la risposta
+    return res;
+  }
+
+  // funzione di decodifica comando utente
+  private static int decode(String res){
+    int ret = 0;
+    switch (res) {
+      case "":   // Stringa vuota
+        ret = 0;
+        break;
+      case ":c": // Comando per cominciare la chat
+        ret = 1;
+        break;
+      case ":h": // Comando per mostrare l'help
+        ret = 2;
+        break;
+      case ":e": // Comando per uscire dal programma
+        ret = 3;
+        break;
+      default:   // Stringa non riconosciuta
+        ret = -1;
+        break;
+    }
+    return ret;
+  }
+
   public static void main(String[] args) throws Exception {
     // Tastiera
     Scanner keyboard = new Scanner(System.in);
@@ -51,21 +95,33 @@ public class SecureServiceClient {
 
     ChatAPI chat = new ChatAPI(username);
     String peer = "";
+    String res = "";
+    int parse = 0;
 
-    System.out.println("Con quale utente vuoi chattare?");
-    peer = keyboard.next();
-
-    chat.connectTo(peer);
-
-    startListener(chat);
-    String msg = "";
-    while (!":quit".equals(msg)) {
-      msg = keyboard.next();
-      if (!("".equals(msg) || ":quit".equals(msg))) {
-	      chat.invia(msg);
+    while(parse < 1){
+      res = help();
+      parse = decode(res);
+      if (parse == 2) {
+        parse = 0;
+        break;
       }
     }
-    // to be continued
+
+    if (parse == 1) {
+      System.out.println("Con quale utente vuoi chattare?");
+      peer = keyboard.next();
+
+      chat.connectTo(peer);
+
+      startListener(chat);
+      String msg = "";
+      while (!":quit".equals(msg)) {
+        msg = keyboard.next();
+        if (!("".equals(msg) || ":quit".equals(msg))) {
+          chat.invia(msg);
+        }
+      }
+    }
     keyboard.close();
   }
 }
