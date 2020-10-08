fdda5bdee7df1b3b440151b3200cbf7509cb1ed2
TheIv <dave.tho@protonmail.com>
2020-10-08 15:01:05 +0000

    aggiunto script all-in-one.sh

---
+++ all-in-one.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Riavvio Tomcat ..."
+./7-shutdownTomcat.sh && ./1-startTomcat.sh
+if [ $? -eq 0 ]; then
+    echo "Tomcat Riavviato!"
+    echo "Creo Server ..."
+    ./2-compileService.sh && ./3-deployAAR.sh
+    if [ $? -eq 0 ]; then
+        echo "Server Creato!"
+        echo "Creo Client ..."
+        ./4-createStub.sh && ./5-compileClient.sh
+        if [ $? -eq 0 ]; then
+            echo "Client creato!"
+            echo "Avvio App ..."
+            ./6-runclient.sh
+        else
+            echo "Errore"
+        fi
+    else
+        echo "Errore"
+    fi
+else
+    echo "Errore"
+fi
