b107ab1383b42631d0bcee0851cf50724a9c01c3
TheIv <dave.tho@protonmail.com>
2020-10-09 11:56:01 +0000

    debug started

--- all-in-one.sh
+++ all-in-one.sh
@@ -8,12 +8,19 @@ if [ $? -eq 0 ]; then
     ./2-compileService.sh && ./3-deployAAR.sh
     if [ $? -eq 0 ]; then
         echo "Server Creato!"
-        echo "Creo Client ..."
-        ./4-createStub.sh && ./5-compileClient.sh
+        echo "Riavvio Tomcat ..."
+        ./7-shutdownTomcat.sh && ./1-startTomcat.sh
         if [ $? -eq 0 ]; then
-            echo "Client creato!"
-            echo "Avvio App ..."
-            ./6-runclient.sh
+            echo "Tomcat Riavviato!"
+            echo "Creo Client ..."
+            ./4-createStub.sh && ./5-compileClient.sh
+            if [ $? -eq 0 ]; then
+                echo "Client creato!"
+                echo "Avvio App ..."
+                ./6-runclient.sh
+            else
+                echo "Errore"
+            fi
         else
             echo "Errore"
         fi
