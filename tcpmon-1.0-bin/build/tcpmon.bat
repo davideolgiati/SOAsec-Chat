eda0711dc46d1c838aefcfdabb44ff9363355e41
Deiv <dave.tho@protonmail.com>
2020-12-01 09:12:27 +0000

    pulizia 1

--- tcpmon-1.0-bin/build/tcpmon.bat
+++
@@ -1,2 +0,0 @@
-rem Run the java exe with tcpmon jar
-java -cp ./tcpmon-1.0.jar org.apache.ws.commons.tcpmon.TCPMon %*
--- tcpmon-1.0-bin/build/tcpmon.sh
+++
@@ -1,1 +0,0 @@
-java -cp ./tcpmon-1.0.jar org.apache.ws.commons.tcpmon.TCPMon $*
